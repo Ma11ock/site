7da272c6bf097d76d996fbe75e587749f7f9a70e
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-01-19 23:42:07 -0800

    Basic handlebars config.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Cropyright (C) Ryan Jeffrey 2022
 // A simple express server that uses handlebars.
 
+
+// TODO convert to typescript
+
 const path = require('path');
 const express = require('express');
 const { engine } = require('express-handlebars');
@@ -22,51 +25,76 @@ function lsTime(timeMS) {
   let fileDate = new Date(timeMS);
   // If the file was updated this year then set the last column to the
   // hour and minute. Else, the last column should be the year.
-  if(Date.now().getFullYear() != fileDate.getFullYear())
+  if((new Date()).getFullYear() != fileDate.getFullYear())
     return `${getMonthByNumber(fileDate.getMonth())} ${fileDate.getDate()} ${fileDate.getHours()}:${fileDate.getMinutes()}`;
   return `${getMonthByNumber(fileDate.getMonth())} ${fileDate.getDate()}  ${fileDate.getFullYear()}`;
 }
 
 function ls(thePath) {
-  let result = "";
+  let result = {};
 
-  fs.stat(thePath, (err, stats) => {
-    // ls file permissions.
-    if(err) {
-      return console.error("Could not stat ", thePath, ": ", err);
-    }
-    // Convert mode to string.
-    let unixFilePermissions = (stats.mode & parseInt('777', 8)).toString(8);
-    // Unix file permission array. The mode is the index in the array.
-    const permStrings = ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'];
-    let permsResult = permStrings[parseInt(unixFilePermissions[0])];
-    permsResult += permStrings[parseInt(unixFilePermissions[1])];
-    permsResult += permStrings[parseInt(unixFilePermissions[2])];
+  let stats = fs.statSync(thePath);
 
-    let prefixChar = '-';
-    if(stats.isDirectory()) {
-      prefixChar = 'd';
-    }
+  // ls file permissions. Convert into an easier format to use.
+  if(!stats) {
+    return console.error("Could not stat", thePath, ":", err);
+  }
+  // Convert mode to string.
+  let unixFilePermissions = (stats.mode & parseInt('777', 8)).toString(8);
+  // Unix file permission array. The mode is the index in the array.
+  const permStrings = ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'];
+  let permsResult = permStrings[parseInt(unixFilePermissions[0])];
+  permsResult += permStrings[parseInt(unixFilePermissions[1])];
+  permsResult += permStrings[parseInt(unixFilePermissions[2])];
 
-    result = `${prefixChar}${permsResult} ${stats.nlink} ryan ryan ${stats.size} ${lsTime(stats.mtimeMS)} ${path.basename(thePath)}`;
-  });
+  let prefixChar = '-';
+  if(stats.isDirectory()) {
+    prefixChar = 'd';
+  }
 
+  result = {
+    perms: `${prefixChar}${permsResult}`,
+    numLinks: stats.nlink,
+    fileSize: stats.size,
+    mtime: lsTime(stats.mtimeMS),
+    basename: path.parse(path.basename(thePath)).name,
+  };
   return result;
 }
 
+// Problem: the site is dynamically generated... so the files we're trying
+// to ls don't exist.
+
+function lsDynamicList(theDir, ext, ...files) {
+  let fileStats = [];
+  files.forEach((element) => {
+    fileStats.push(ls(path.join(theDir, element + ext)));
+  });
+  console.log(fileStats);
+  return fileStats;
+}
+
+function lsList(theDir, ...files) {
+  let fileStats = [];
+  files.forEach((element) => {
+    fileStats.push(ls(path.join(theDir, element)));
+  });
+  return fileStats;
+}
+
 function lsDir(thePath) {
-  let lines = [];
+  let fileStats = [];
   fs.readdir(thePath, (err, files) => {
     if(err) {
       return console.log('Cannot scane directory ', thePath, ": ", err);
     }
 
     files.forEach((file) => {
-      lines.push(ls(file));
+      fileStats.push(ls(file));
     });
   });
 
-  return lines;
+  return fileStats;
 }
 
 // App config
@@ -99,8 +127,10 @@ app.get('/posts', (req, res, next) => {
 
 
 app.get('/', (req, res, next) => {
+  let test = lsDynamicList('public', '.html', 'main', 'software', 'sneed');
+  console.log(test);
   res.status(200).render('index', {
-    entries: lsDir('.'),
+    entries: test
   });
 });
 
