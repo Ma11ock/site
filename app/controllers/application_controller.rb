18a03b96cc885312d47303cb00ea2af0e3d60914
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-02-23 00:03:59 -0800

    Random ascii comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  def self.random_comment
+    File.join('shared', ['sneed', 'industrial_society', 'anime', 'choppa',
+                         'france', 'gigachad', 'peter', 'shrek', 'sneed_img',
+                         'tanku', 'toem', 'troll', 'virus_exe', 'windows'].sample)
+  end
 end
