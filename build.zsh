9ca3bafbdd25ba39d7689a97361704affcb498f1
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-02-23 21:32:29 -0800

    Fix build script for remote server

--- build.zsh
+++ build.zsh
@@ -4,7 +4,7 @@
 # TODO rake db:create RAILS_ENV=production
 # TODO rake db:migrate RAILS_ENV=production
 export RAILS_ENV=production
-bin/bundle install
-bin/bundle exec rake assets:precompile
-bin/bundle exec rake assets:clean
-bin/bundle exec rake db:migrate
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
